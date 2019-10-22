5aaba4da297a027002a4420f58b28c05cf54178b
ChristoPedro <pedroegcj@hotmail.com>
2019-10-22 19:05:58 -0300

    commit to master

--- twelves.py
+++ twelves.py
@@ -15,8 +15,22 @@ def get_cursos(tag):
     querystring = {"profissao":profissao}
     headers = {'Authorization': "Basic b2ljZGVtb3VzZXI6T3JhY2xlMTIzNDU2"}
     lista = json.loads(requests.request("GET", url, headers=headers, params=querystring))"""
-    lista = [1,tag,3]
+    lista = json.loads('{"items":[{"key_word":"CD","Question":"SQL","Father":"CD","Sort":1},{"key_word":"CD","Question":"RP","Father":"CD","Sort":2},{"key_word":"CD","Question":"Estatistica","Father":"CD","Sort":3},{"key_word":"CD","Question":"MOD","Father":"Estatistica","Sort":4}]}')['items']
     return lista
+
+def get_perguntas(param, lista, ordem):
+    length = len(lista)
+    prox = ordem + 1
+    if param != 'Sim':
+        for i in range(ordem,length):
+            if i <= len(lista[i]) is not None:
+                if lista[i]['Father'] == lista[ordem]['Question']:
+                    lista.pop(i)
+        session['trilha'].append(lista[ordem]['Question'])
+    if ordem >= (len(lista) - 1):
+        return 'Essa é a sua Trilha:' + str(session['trilha'])
+    session['ordem'] = prox
+    return 'Você Conhece ' + lista[prox]['Question'] + '?'
 
 #Cria o path relativo do endpoint
 @app.route('/tag', methods=['GET', 'POST'])
@@ -24,10 +38,12 @@ def tag():
     #verifica se a chamada é um GET ou um POST
     if request.method == 'GET':
         session['lista'] = get_cursos(request.args.get('tag'))
-        return str(session['lista'][0])
+        session['ordem'] = 0
+        session['trilha'] = []
+        return 'Você Conhece ' + session['lista'][0]['Question'] + '?'
     else:
         if session.get('lista') is not None:
-            return str(session.get('lista')[1])
+            return get_perguntas(request.args.get('resposta'), session['lista'], session['ordem'])
         else:
             return 'Não Existe Nenhuma sessão'
 
