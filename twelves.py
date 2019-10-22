68ab9b6e8e65dd28d76f64faaf6ea133e79f9041
ChristoPedro <pedroegcj@hotmail.com>
2019-10-22 14:45:41 -0300

    commit on master

--- twelves.py
+++ twelves.py
@@ -5,29 +5,31 @@ import os
 import json
 
 app = Flask(__name__)
+#Cria uma chave aletória para cada sessão
 app.secret_key = os.urandom(20)
 
-"""def get_cursos(profissao):
+def get_cursos(tag):
 
-    url = "https://OIC-DIGIDEV-ladsedigdev.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/HELLO_WORLD/1.0/names"
+    """url = "https://OIC-DIGIDEV-ladsedigdev.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/HELLO_WORLD/1.0/names"
     querystring = {"profissao":profissao}
     headers = {'Authorization': "Basic b2ljZGVtb3VzZXI6T3JhY2xlMTIzNDU2"}
-    lista = json.loads(requests.request("GET", url, headers=headers, params=querystring))
+    lista = json.loads(requests.request("GET", url, headers=headers, params=querystring))"""
+    lista = [1,tag,3]
     return lista
-"""
 
-@app.route('/set')
-def set():
-    prof = request.args.get('id')
-    session['id'] = prof
-    return session['id']
-
-@app.route('/get')
-def get():
-    if session.get('id') is not None:
-        return session.get('id')
+#Cria o path relativo do endpoint
+@app.route('/tag', methods=['GET', 'POST'])
+def tag():
+    #verifica se a chamada é um GET ou um POST
+    if request.method == 'GET':
+        session['lista'] = get_cursos(request.args.get('tag'))
+        return str(session['lista'][0])
     else:
-        return 'não tem sessão'
+        if session.get('lista') is not None:
+            return str(session.get('lista')[1])
+        else:
+            return 'Não Existe Nenhuma sessão'
+
 
 if __name__ == '__main__':
     
