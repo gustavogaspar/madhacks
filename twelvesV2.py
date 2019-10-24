8f51dab1cad1612636b519883d4cf841fdc22ad9
ChristoPedro <pedroegcj@hotmail.com>
2019-10-24 10:48:10 -0300

    commit to master

--- twelvesV2.py
+++ twelvesV2.py
@@ -11,29 +11,33 @@ app.secret_key = os.urandom(20)
 #funcao que traz todos os cursos associadas a tag pesquisada
 def get_cursos(tag):
 
-    """url = "https://OIC-DIGIDEV-ladsedigdev.integration.ocp.oraclecloud.com:443/ic/api/integration/v1/flows/rest/HELLO_WORLD/1.0/names"
-    querystring = {"profissao":profissao}
-    headers = {'Authorization': "Basic b2ljZGVtb3VzZXI6T3JhY2xlMTIzNDU2"}
-    lista = json.loads(requests.request("GET", url, headers=headers, params=querystring))"""
-    lista = json.loads('{"items":[{"key_word":"CD","Question":"SQL","Father":"CD","Sort":1},{"key_word":"CD","Question":"RP","Father":"CD","Sort":2},{"key_word":"CD","Question":"Estatistica","Father":"CD","Sort":3},{"key_word":"CD","Question":"MOD","Father":"Estatistica","Sort":3}]}')['items']
+    url = "https://eybftnzpirfzozn-siriusdb.adb.us-ashburn-1.oraclecloudapps.com/ords/madhacks/sirius/key_word"
+    querystring = {"KEYWORD":tag}
+    response = requests.request("GET", url, params=querystring)
+    lista = json.loads(response.text)['items']
     return lista
 
 #fucao que traz as perguntas na ordem, verifica se a resposta foi sim ou nao 
 #adiciona os topicos com resposta negativa na trilha e remove os filhos desse topico
 def get_perguntas(param, lista, pai, sort, ordem):
-    perguntas = [t for t in lista if t['Sort'] == sort]
+    perguntas = [t for t in lista if t['sort'] == sort]
     if param != 'Sim':
-        session['trilha'].append(perguntas[ordem]['Question'])
-        for i in range(ordem, len(perguntas)):
-            if ordem <= len(perguntas) - 1:
-                if perguntas[i]['Father'] == pai:
-                    sort += 1
-                    ordem = 0
-                else:
-                    ordem += 1
-            else:
+        if ordem <= len(perguntas) - 1:
+            if perguntas[ordem]['father'] == pai:
+                result = [p for p in perguntas if p['father'] == perguntas[ordem]['question']]
+                for j in result:
+                    session['trilha'].append(j['id_question_pk'])
                 sort += 1
                 ordem = 0
+            if ordem == len(perguntas) - 1:
+                ordem = 0
+                sort += 1
+            else:
+                session['trilha'].append(perguntas[ordem]['id_question_pk'])
+                ordem += 1
+        else:
+            sort += 1
+            ordem = 0
     else:
         if ordem <= len(perguntas) - 1:
             if ordem == len(perguntas) - 1:
@@ -41,8 +45,8 @@ def get_perguntas(param, lista, pai, sort, ordem):
                 ordem = 0
             else:
                 ordem += 1
-    if max(t['Sort'] for t in lista) >= sort:
-        resposta = 'Você conhece ' + str([t['Question'] for t in lista if t['Sort'] == sort][ordem]) + '?'
+    if max(t['sort'] for t in lista) >= sort:
+        resposta = 'Você conhece ' + str([t['question'] for t in lista if t['sort'] == sort][ordem]) + '?'
         return resposta, ordem, sort
     else:
         return 'Essa é sua lista ' + str(session['trilha']), 0 ,1 
@@ -58,7 +62,7 @@ def tag():
             session['sort'] = 1
             session['trilha'] = []
             session['ordem'] = 0
-            pergunta = str([t['Question'] for t in session['lista'] if t['Sort'] == session['sort'] and t['Father'] == session['father']][0])
+            pergunta = str([t['question'] for t in session['lista'] if t['sort'] == session['sort'] and t['father'] == session['father']][0])
             return 'Você Conhece ' + pergunta + '?'
         else: 
             return 'Profissão não encontrada'
