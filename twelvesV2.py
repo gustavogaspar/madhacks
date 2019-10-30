cb04b3618f07ad2692cd7cb18f44bf4a122530be
ChristoPedro <pedroegcj@hotmail.com>
2019-10-30 18:08:23 -0300

    Go Twelves

--- twelvesV2.py
+++ twelvesV2.py
@@ -3,6 +3,7 @@ from flask_session import Session
 import requests
 import os
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 #Cria uma chave aletoria para cada sessao
@@ -11,7 +12,7 @@ app.secret_key = os.urandom(20)
 #funcao que traz todas as perguntas
 def get_todas_perguntas(tag):
 
-    url = "https://eybftnzpirfzozn-siriusdb.adb.us-ashburn-1.oraclecloudapps.com/ords/madhacks/sirius/key_word"
+    url = "https://nrlyqybsaqsrbvj-siriusatp.adb.us-ashburn-1.oraclecloudapps.com/ords/SIRIUS/madkhacks/questions"
     querystring = {"KEYWORD":tag}
     response = requests.request("GET", url, params=querystring)
     lista = json.loads(response.text)['items']
@@ -21,31 +22,35 @@ def get_todas_perguntas(tag):
 def get_recomendacao(respostas):
 
     recomendacao = []
-    url = "https://eybftnzpirfzozn-siriusdb.adb.us-ashburn-1.oraclecloudapps.com/ords/madhacks/sirius/recomendation"
+    valorTotal = 0
+    url = "https://nrlyqybsaqsrbvj-siriusatp.adb.us-ashburn-1.oraclecloudapps.com/ords/SIRIUS/madkhacks/recomendacoes"
     for f in respostas:
         querystring = {"id":f[0], "resposta":f[1]}
         req = json.loads(requests.request("GET", url, params=querystring).text)['items']
         for i in req:
             recomendacao.append(i)
-    return recomendacao
+            valorTotal+= i['price']
+
+    return recomendacao, valorTotal
 
 #fucao que traz as perguntas na ordem, verifica se a resposta foi sim ou nao 
 #adiciona os topicos com resposta negativa na trilha e remove os filhos desse topico
-def get_perguntas(param, lista, pai, sort, ordem):
+def get_perguntas(param, lista, sort, ordem):
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     perguntas = [t for t in lista if t['sort'] == sort]
     if ordem <= len(perguntas) - 1:
-        if perguntas[ordem]['father'] == pai:
+        if perguntas[ordem]['father'] == 0:
             if param == 'No':
-                session['trilha'].append([perguntas[ordem]['id'], param])
-                result = [p for p in perguntas if p['father'] != pai]
+                session['trilha'].append([perguntas[ordem]['id'], param,now])
+                result = [p for p in perguntas if p['father'] != 0]
                 if len(result) > 0: 
                     for j in result:
-                        session['trilha'].append([j['id'],j['respostas'].split(',')[-1]])
+                        session['trilha'].append([j['id'],j['respostas'].split(',')[-1],now])
                 sort += 1
                 ordem = 0        
             else:
-                session['trilha'].append([perguntas[ordem]['id'], param])
-                result = [p for p in perguntas if p['father'] != pai]
+                session['trilha'].append([perguntas[ordem]['id'], param,now])
+                result = [p for p in perguntas if p['father'] != 0]
                 if len(result) > 0:
                     ordem += 1
                 else:
@@ -53,10 +58,10 @@ def get_perguntas(param, lista, pai, sort, ordem):
                     sort += 1
         else:
             if ordem < len(perguntas) -1:
-                session['trilha'].append([perguntas[ordem]['id'], param])
+                session['trilha'].append([perguntas[ordem]['id'], param,now])
                 ordem += 1
             else:
-                session['trilha'].append([perguntas[ordem]['id'], param])
+                session['trilha'].append([perguntas[ordem]['id'], param,now])
                 ordem = 0
                 sort += 1
     if sort <= max(t['sort'] for t in lista):
@@ -65,21 +70,21 @@ def get_perguntas(param, lista, pai, sort, ordem):
         retorno = {"Pergunta" : pergunta, "Respostas": respostas.split(',')}
         return retorno, ordem, sort
     else:
-        return {"Recomendacao":get_recomendacao(session['trilha'])}, 0 ,1 
+        recomendacao, valor = get_recomendacao(session['trilha'])
+        return {"Recomendacao": recomendacao, "Valor Total": valor}, 0 ,1 
 
 #Cria o path relativo do endpoint
 @app.route('/tag', methods=['GET', 'POST'])
 def tag():
     #verifica se a chamada é um GET ou um POST
     if request.method == 'GET':
-        session['father'] = request.args.get('tag')
-        session['lista'] = get_todas_perguntas(session['father'])
+        session['lista'] = get_todas_perguntas(request.args.get('tag'))
         if len(session['lista']) > 0:
             session['sort'] = 1
             session['trilha'] = []
             session['ordem'] = 0
-            pergunta = [t['question'] for t in session['lista'] if t['sort'] == session['sort'] and t['father'] == session['father']][0]
-            respostas = [t['respostas'] for t in session['lista'] if t['sort'] == session['sort'] and t['father'] == session['father']][0]
+            pergunta = [t['question'] for t in session['lista'] if t['sort'] == session['sort'] and t['father'] == 0][0]
+            respostas = [t['respostas'] for t in session['lista'] if t['sort'] == session['sort'] and t['father'] == 0][0]
             return {"Pergunta": pergunta, "Resposta": respostas.split(',')}
         else: 
             return 'Profissão não encontrada'
@@ -87,7 +92,7 @@ def tag():
         if session.get('lista') is not None:
             resposta = ''
             arg = request.get_json()
-            resposta, session['ordem'], session['sort'] = get_perguntas(arg['resposta'], session['lista'], session['father'], session['sort'], session['ordem'])
+            resposta, session['ordem'], session['sort'] = get_perguntas(arg['resposta'], session['lista'], session['sort'], session['ordem'])
             return resposta
         else:
             return 'Não Existe Nenhuma sessão'
@@ -95,4 +100,4 @@ def tag():
 
 if __name__ == '__main__':
     
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, port=5000)
