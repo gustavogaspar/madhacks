5d8ca2d11b3edba2a9a29e2807c8cb4cf64ea6e0
ChristoPedro <pedroegcj@hotmail.com>
2019-11-05 10:24:33 -0300

    Ultima

--- twelvesV2.py
+++ twelvesV2.py
@@ -9,7 +9,7 @@ import flask_cors
 app = Flask(__name__)
 #Cria uma chave aletoria para cada sessao
 app.secret_key = os.urandom(12).hex()
-CORS(app,allow_headers=['Cookie'])
+CORS(app,allow_headers=['Cookie', 'Content-Type', 'Access-Control-Allow-Credentials'])
 
 #funcao que traz todas as perguntas
 def get_todas_perguntas(tag):
